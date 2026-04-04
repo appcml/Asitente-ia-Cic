ec6bb814b56846dc5f6025017ae12c7fc33984ea
appcml <35844750+appcml@users.noreply.github.com>
2026-04-03 21:28:41 -0300

    Update __init__.py

--- bebe_ia/core/__init__.py
+++ bebe_ia/core/__init__.py
@@ -4,7 +4,7 @@ Núcleo del Bebé IA
 from .config import Config
 from .tokenizer import SimpleTokenizer
 from .model import BebeTransformer
-from .memory import MemorySystem
+from .memory import ContinualLearner as MemorySystem
 from .learner import ContinualLearner
 from .personality import BebePersonality
 
