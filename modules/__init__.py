b22a71bf2fc7bea899f2810530bdc39592359018
appcml <35844750+appcml@users.noreply.github.com>
2026-04-09 17:03:36 -0400

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,17 @@
+"""
+Módulos especializados de Cic_IA
+"""
+
+from .data_analysis import DataAnalysisModule
+from .image_generator import ImageGeneratorModule
+from .code_assistant import CodeAssistantModule
+from .chat_history import ChatHistoryModule
+from .file_manager import FileManagerModule
+
+__all__ = [
+    'DataAnalysisModule',
+    'ImageGeneratorModule', 
+    'CodeAssistantModule',
+    'ChatHistoryModule',
+    'FileManagerModule'
+]
