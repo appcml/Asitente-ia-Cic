d7e359c283459bae734da91376fef618896898e8
appcml <35844750+appcml@users.noreply.github.com>
2026-06-06 22:56:08 -0400

    Update main.py

--- modules/code_executor/main.py
+++ modules/code_executor/main.py
@@ -1,1 +1,10 @@
+# main.py
 
+from flask import Flask
+from codex.routes import codex_blueprint
+
+app = Flask(__name__)
+app.register_blueprint(codex_blueprint)
+
+if __name__ == '__main__':
+    app.run(debug=True)
