ed2e30157845abf1475403bb918342e40563e230
appcml <35844750+appcml@users.noreply.github.com>
2026-06-20 17:59:17 -0400

    Update __init__.py

--- modules/video_gen/__init__.py
+++ modules/video_gen/__init__.py
@@ -1,1 +1,5 @@
+# modules/video_gen/__init__.py
+from modules.video_gen.main import VideoGeneratorModule
+from modules.video_gen.routes import register_video_routes
 
+__all__ = ["VideoGeneratorModule", "register_video_routes"]
